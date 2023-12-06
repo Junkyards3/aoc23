96b783249b7475684daeb46716705d2328276b42
hfavereau <thehwanderer@gmail.com>
2023-12-06 20:52:09 +0100

    day5 - remove unused code

--- src/days/day5.rs
+++ src/days/day5.rs
@@ -1,10 +1,11 @@
 use std::fs::File;
 use std::io::BufRead;
+use std::ops::Range;
 use std::str::FromStr;
 
 use crate::days::Day;
 
-type Quantity = u64;
+type Quantity = i128;
 
 pub struct Day5 {
     start_seeds: Vec<Quantity>,
@@ -12,12 +13,6 @@ pub struct Day5 {
 }
 
 impl Day5 {
-    fn contains_seed(&self, seed: Quantity) -> bool {
-        self.start_seeds
-            .chunks(2)
-            .any(|chunk| (chunk[0]..chunk[0] + chunk[1]).contains(&seed))
-    }
-
     fn convert_quantity(&self, seed: Quantity) -> Quantity {
         self.transformers
             .iter()
@@ -26,15 +21,14 @@ impl Day5 {
             })
     }
 
-    fn convert_quantity_reverse(&self, location: Quantity) -> Quantity {
-        self.transformers
-            .iter()
-            .rev()
-            .fold(location, |curr_quantity, transformer| {
-                transformer.convert_quantity_reverse(curr_quantity)
-            })
+    fn get_ranges(&self) -> Vec<Range<Quantity>> {
+        self.start_seeds
+            .chunks(2)
+            .map(|chunk| chunk[0]..chunk[0] + chunk[1])
+            .collect()
     }
 }
+
 #[derive(Debug)]
 struct Transformer {
     transformer_lines: Vec<TransformerLine>,
@@ -53,16 +47,36 @@ impl Transformer {
         quantity
     }
 
-    fn convert_quantity_reverse(&self, quantity: Quantity) -> Quantity {
-        for transformer_line in self.transformer_lines.iter() {
-            if quantity >= transformer_line.destination_start
-                && quantity < transformer_line.destination_start + transformer_line.length
-            {
-                return quantity + transformer_line.source_start
-                    - transformer_line.destination_start;
+    fn map_range(&self, range: Range<Quantity>) -> Vec<Range<Quantity>> {
+        let mut mapped_ranges = vec![];
+        let mut remaining_ranges = vec![range];
+        for line in self.transformer_lines.iter() {
+            let mut new_ranges = vec![];
+            for remaining_range in remaining_ranges {
+                let mut transformer_result = line.map_range(remaining_range);
+                if let Some(mapped_range) = transformer_result.mapped_range {
+                    mapped_ranges.push(mapped_range);
+                }
+                new_ranges.append(&mut transformer_result.remaining_ranges);
             }
+            remaining_ranges = new_ranges;
         }
-        quantity
+        mapped_ranges.append(&mut remaining_ranges);
+        merge_ranges(&mut mapped_ranges);
+        mapped_ranges
+    }
+}
+
+fn merge_ranges(ranges: &mut Vec<Range<Quantity>>) {
+    ranges.sort_unstable_by_key(|range| range.start);
+    let mut idx = 0;
+    while idx < ranges.len() - 1 {
+        if ranges[idx].end >= ranges[idx + 1].start {
+            ranges[idx].end = ranges[idx + 1].end;
+            ranges.remove(idx + 1);
+        } else {
+            idx += 1;
+        }
     }
 }
 
@@ -73,6 +87,40 @@ struct TransformerLine {
     length: Quantity,
 }
 
+#[derive(Debug)]
+struct RangeTransformResult {
+    mapped_range: Option<Range<Quantity>>,
+    remaining_ranges: Vec<Range<Quantity>>,
+}
+
+impl TransformerLine {
+    fn map_range(&self, range: Range<Quantity>) -> RangeTransformResult {
+        let left_part = range.start.min(self.source_start)..range.end.min(self.source_start);
+        let right_part = range.start.max(self.source_start + self.length)
+            ..range.end.max(self.source_start + self.length);
+        let inner_part =
+            range.start.max(self.source_start)..range.end.min(self.source_start + self.length);
+        let diff = self.destination_start - self.source_start;
+
+        let mut remaining_ranges = vec![];
+        if !left_part.is_empty() {
+            remaining_ranges.push(left_part);
+        }
+        if !right_part.is_empty() {
+            remaining_ranges.push(right_part);
+        }
+
+        let mapped_range = if inner_part.is_empty() {
+            None
+        } else {
+            Some(inner_part.start + diff..inner_part.end + diff)
+        };
+        RangeTransformResult {
+            mapped_range,
+            remaining_ranges,
+        }
+    }
+}
 impl FromStr for TransformerLine {
     type Err = ();
 
@@ -140,7 +188,7 @@ impl Day for Day5 {
     }
 
     fn solution2(&self) -> String {
-        let mut result = 0;
+        /*let mut result = 0;
         loop {
             let seed = self.convert_quantity_reverse(result);
             if self.contains_seed(seed) {
@@ -148,6 +196,21 @@ impl Day for Day5 {
             }
             result += 1;
         }
+        result.to_string()*/
+        let seed_ranges = self.get_ranges();
+        let result = self
+            .transformers
+            .iter()
+            .fold(seed_ranges, |ranges, transformer| {
+                ranges
+                    .into_iter()
+                    .flat_map(|range| transformer.map_range(range))
+                    .collect()
+            })
+            .iter()
+            .map(|range| range.start)
+            .min()
+            .unwrap();
         result.to_string()
     }
 }
