9b7da1e387bc7187e59d3b595df1f344138765e0
hfavereau <thehwanderer@gmail.com>
2023-12-12 23:08:34 +0100

    day12 (inspired by sergiosgc)

--- src/days/day12.rs
+++ src/days/day12.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::BufRead;
 
@@ -10,19 +11,114 @@ pub struct Day12 {
 
 #[derive(Debug)]
 struct SpringLine {
-    groups: Vec<ContiguousGroup>,
+    groups: Vec<SpringElement>,
     goal: Vec<usize>,
 }
 
 impl SpringLine {
-    fn find_ways_to_fit(&self, layouts: &Vec<Vec<SpringLayout>>) -> usize {
-        let mut curr_goal_idx = 0;
-        for group in self.groups.iter() {
-            /*let curr_goal = self.goal[curr_goal_idx];
-            let compatible_layouts = layouts[group.]*/
-        }
-        0
+    fn find_ways_to_fit(&self) -> usize {
+        let mut cache = HashMap::new();
+        find_ways_to_fit_recursive(&self.groups, &self.goal, &mut cache)
     }
+
+    fn find_ways_to_fit_unfold(&self) -> usize {
+        let mut groups_with_sep = self.groups.clone();
+        groups_with_sep.push(SpringElement::Unknown);
+        let unfolded_groups = groups_with_sep
+            .into_iter()
+            .cycle()
+            .take(5 * self.groups.len() + 4)
+            .collect::<Vec<_>>();
+        let unfolded_goal = self
+            .goal
+            .clone()
+            .into_iter()
+            .cycle()
+            .take(5 * self.goal.len())
+            .collect::<Vec<_>>();
+        let mut cache = HashMap::new();
+        find_ways_to_fit_recursive(&unfolded_groups, &unfolded_goal, &mut cache)
+    }
+}
+
+enum BrokenSeqResult<'a> {
+    FailureToForm,
+    SuccessWithSpringsRemaining(&'a [SpringElement]),
+    SuccessWithSpringsEnd,
+}
+
+fn form_broken_seq(springs: &[SpringElement], goal: usize) -> BrokenSeqResult {
+    for index in 0..goal {
+        match springs.get(index) {
+            None | Some(SpringElement::Working) => {
+                return BrokenSeqResult::FailureToForm;
+            }
+            Some(SpringElement::Broken) | Some(SpringElement::Unknown) => {}
+        }
+    }
+
+    if springs
+        .get(goal)
+        .is_some_and(|spring| *spring == SpringElement::Broken)
+    {
+        BrokenSeqResult::FailureToForm
+    } else {
+        if springs.len() < goal + 1 {
+            BrokenSeqResult::SuccessWithSpringsEnd
+        } else {
+            BrokenSeqResult::SuccessWithSpringsRemaining(&springs[goal + 1..])
+        }
+    }
+}
+
+type CacheKey = (Vec<SpringElement>, Vec<usize>);
+
+fn find_ways_to_fit_recursive(
+    mut left_springs: &[SpringElement],
+    left_goals: &[usize],
+    cache: &mut HashMap<CacheKey, usize>,
+) -> usize {
+    while left_springs
+        .get(0)
+        .is_some_and(|spring| *spring == SpringElement::Working)
+    {
+        left_springs = &left_springs[1..];
+    }
+
+    if left_springs.is_empty() {
+        return left_goals.is_empty().into();
+    }
+
+    if left_goals.is_empty() {
+        return left_springs
+            .iter()
+            .all(|spring| *spring != SpringElement::Broken)
+            .into();
+    }
+    let key = (left_springs.to_vec(), left_goals.to_vec());
+
+    if let Some(count) = cache.get(&key) {
+        return *count;
+    }
+
+    let count_with_broken = match form_broken_seq(&left_springs, left_goals[0]) {
+        BrokenSeqResult::FailureToForm => 0,
+        BrokenSeqResult::SuccessWithSpringsRemaining(remaining_springs) => {
+            find_ways_to_fit_recursive(remaining_springs, &left_goals[1..], cache)
+        }
+        BrokenSeqResult::SuccessWithSpringsEnd => {
+            find_ways_to_fit_recursive(&[], &left_goals[1..], cache)
+        }
+    };
+
+    let result = if left_springs[0] == SpringElement::Unknown {
+        let count_with_working = find_ways_to_fit_recursive(&left_springs[1..], left_goals, cache);
+        count_with_working + count_with_broken
+    } else {
+        count_with_broken
+    };
+    cache.insert(key, result);
+    result
 }
 
 impl From<String> for SpringLine {
@@ -30,11 +126,7 @@ impl From<String> for SpringLine {
         let (springs_str, goal_str) = value
             .split_once(' ')
             .expect("should separate springs and goals");
-        let groups = springs_str
-            .split('.')
-            .filter(|r| !r.is_empty())
-            .map(|g| g.into())
-            .collect();
+        let groups = springs_str.chars().map(|g| g.into()).collect();
         let goal = goal_str
             .split(',')
             .map(|nb_str| nb_str.parse().expect("should be a number"))
@@ -44,112 +136,21 @@ impl From<String> for SpringLine {
     }
 }
 
-#[derive(Debug)]
-struct ContiguousGroup {
-    elements: Vec<(GroupElement, usize)>,
-}
-
-impl From<&str> for ContiguousGroup {
-    fn from(value: &str) -> Self {
-        let mut chars = value.chars();
-        let mut elements = vec![];
-        let mut actual_group_size = 1;
-        let mut actual_group_element: GroupElement =
-            chars.next().expect("should have a char").into();
-        for c in chars {
-            let new_group = c.into();
-            if new_group == actual_group_element {
-                actual_group_size += 1;
-            } else {
-                elements.push((actual_group_element, actual_group_size));
-                actual_group_size = 1;
-                actual_group_element = new_group;
-            }
-        }
-        elements.push((actual_group_element, actual_group_size));
-        ContiguousGroup { elements }
-    }
-}
-
-#[derive(Debug, Eq, PartialEq)]
-enum GroupElement {
+#[derive(Debug, Eq, PartialEq, Hash, Clone)]
+enum SpringElement {
     Unknown,
-    Spring,
+    Broken,
+    Working,
 }
 
-impl From<char> for GroupElement {
+impl From<char> for SpringElement {
     fn from(value: char) -> Self {
         match value {
-            '#' => GroupElement::Spring,
-            '?' => GroupElement::Unknown,
+            '#' => SpringElement::Broken,
+            '?' => SpringElement::Unknown,
+            '.' => SpringElement::Working,
             _ => panic!("wrong char"),
         }
-    }
-}
-#[derive(Debug)]
-struct SpringLayout {
-    size: usize,
-    touch_left: bool,
-    touch_right: bool,
-    spring_groups: Vec<usize>,
-}
-
-impl SpringLayout {
-    fn compute_size_one_layouts() -> Vec<Self> {
-        vec![
-            SpringLayout {
-                size: 1,
-                touch_left: false,
-                touch_right: false,
-                spring_groups: vec![],
-            },
-            SpringLayout {
-                size: 1,
-                touch_left: true,
-                touch_right: true,
-                spring_groups: vec![1],
-            },
-        ]
-    }
-
-    fn compute_next_size_layouts(size_layout: &Vec<SpringLayout>) -> Vec<SpringLayout> {
-        let previous_size = size_layout[0].size;
-        size_layout
-            .iter()
-            .flat_map(|layout| {
-                let mut extended_groups = layout.spring_groups.clone();
-                if layout.touch_right {
-                    *extended_groups
-                        .last_mut()
-                        .expect("there is a last element if touch_right is true") += 1;
-                } else {
-                    extended_groups.push(1)
-                }
-                let extend_with_spring = SpringLayout {
-                    size: previous_size + 1,
-                    touch_left: layout.touch_left,
-                    touch_right: true,
-                    spring_groups: extended_groups,
-                };
-                let extend_without_spring = SpringLayout {
-                    size: previous_size + 1,
-                    touch_left: layout.touch_left,
-                    touch_right: false,
-                    spring_groups: layout.spring_groups.clone(),
-                };
-                vec![extend_with_spring, extend_without_spring]
-            })
-            .collect()
-    }
-
-    fn compute_all_layouts_below_size(size: usize) -> Vec<Vec<SpringLayout>> {
-        let mut result = Vec::with_capacity(size);
-        result.push(SpringLayout::compute_size_one_layouts());
-        for _ in 2..=size {
-            let curr_layouts = SpringLayout::compute_next_size_layouts(result.last().unwrap());
-            result.push(curr_layouts);
-        }
-        result.into_iter().collect()
     }
 }
 
@@ -163,11 +164,21 @@ impl Day for Day12 {
     }
 
     fn solution1(&self) -> String {
-        "sol1".to_string()
+        let result = self
+            .lines
+            .iter()
+            .map(|line| line.find_ways_to_fit())
+            .sum::<usize>();
+        result.to_string()
     }
 
     fn solution2(&self) -> String {
-        "sol2".to_string()
+        let result = self
+            .lines
+            .iter()
+            .map(|line| line.find_ways_to_fit_unfold())
+            .sum::<usize>();
+        result.to_string()
     }
 }
 
@@ -179,13 +190,29 @@ mod tests {
     fn test_day12_sol1() {
         let input = File::open("./inputs/day12/input_test.txt").expect("File not found");
         let day = Day12::make_day(input);
-        assert_eq!(day.solution1(), "sol1");
+        assert_eq!(day.solution1(), "21");
+    }
+
+    #[test]
+    fn test_find_ways() {
+        let g1: SpringLine = "#.??. 1".to_string().into();
+        let mut cache = HashMap::new();
+        let v = find_ways_to_fit_recursive(&g1.groups, &g1.goal, &mut cache);
+        dbg!(cache);
+        assert_eq!(v, 1);
     }
 
     #[test]
     fn test_day12_sol2() {
         let input = File::open("./inputs/day12/input_test.txt").expect("File not found");
         let day = Day12::make_day(input);
-        assert_eq!(day.solution2(), "sol2");
+        assert_eq!(day.solution2(), "525152");
+    }
+
+    #[test]
+    fn test_find_ways_2() {
+        let g1: SpringLine = "????.#...#... 4,1,1".to_string().into();
+        let v = g1.find_ways_to_fit_unfold();
+        assert_eq!(v, 16);
     }
 }
